d0e62de343aa60244683f513fdd28773b702058c
InvertedTNT <tzbodula@gmail.com>
2020-05-14 12:33:54 -0400

    Created Two String Variables

--- Go/HelloWorld!.go
+++ Go/HelloWorld!.go
@@ -2,7 +2,17 @@ package main
 
 import "fmt"
 
+/*
+Program created by Thomas Zbodula
+*/
+
 func main() {
-	fmt.Println("My name is Thomas Zbodula.")
-	fmt.Println("I currently live in Charlotte, North Carolina")
+
+	fmt.Println("My name is Thomas Zbodula.")                    //Displays name
+	fmt.Println("I currently live in Charlotte, North Carolina") //Displays location
+
+	var firstName = "Robert"
+	var lastName = "Johnson"
+
+	fmt.Println("Someone's Name Is " + firstName + lastName)
 }
